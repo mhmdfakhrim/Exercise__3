a1745861e90ffb1540882ee473b5df89c5d726b3
mhmdfakhrim <muhfakhri4@gmail.com>
2022-11-21 16:21:17 +0700

    exercise

--- Program.cs
+++ Program.cs
@@ -66,9 +66,31 @@ namespace Exercise__3
             else
                 Console.WriteLine("\nThe first record in the list is:\n\n " + LAST.next.rollNumber + "   " + LAST.next.name);
         }
+
     }
     static void Main(string[] args)
+    {
+        CircularList obj = new CircularList();
+        while (true)
         {
+            try
+            {
+                Console.WriteLine("\nMenu");
+                Console.WriteLine("1. View all the records in the list");
+                Console.WriteLine("2. Search for a record in the list");
+                Console.WriteLine("3. Display the first record int the list");
+                Console.WriteLine("4. Exit");
+                Console.Write("\nEnter your choice (1-4): ");
+                char ch = Convert.ToChar(Console.ReadLine());
+                switch (ch)
+                {
+                    case '1':
+                        {
+
+                        }
+                }
+            }
+        }
         }
 }
 
